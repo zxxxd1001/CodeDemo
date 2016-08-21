0b12e5cfacc4512b9365025979719e1d43fea151
zhangxd <zxxxd1001@126.com>
2016-08-21 20:11:36 +0800

    [张雪冬]

--- src/main/webapp/javaScript/basis/Inject-method.js
+++ src/main/webapp/javaScript/basis/Inject-method.js
@@ -1,11 +1,11 @@
 var exampleApp = angular.module('exampleApp',[]);
 exampleApp.controller('exampleController',['$scope', function($scope){
-    $scope.name = "boyi";
-    $scope.inject = function(){
+    $scope.name = "张三";
+    $scope.testInject = function(){
         var $injector = angular.injector(['ng']);
         $injector.invoke(function($http) {
             var scopes = angular.element(document.body).scope();
-            scopes.name = "博弈网络";//这里可以同http请求获得数据
+            scopes.name = "李四";//这里可以同http请求获得数据
         });
     };
 }]);
--- src/main/webapp/javaScript/basis/my-serve.js
+++ src/main/webapp/javaScript/basis/my-serve.js
@@ -1,4 +1,50 @@
-angular.module("myServeApp",[]);
-angular.module("myServeApp").controller("myServeController",["$scope",function($scope){
-    
+angular.module('myServeApp',[],function($provide, $filterProvider,$controllerProvider){
+    //自定义服务
+    $provide.provider('Service',function(){
+        this.$get=function(){
+            return{
+                messagr:"Service messagr"
+            }
+        }
+    });
+    //可以有多个服务
+    $provide.provider('Service2',function(){
+        this.$get=function(){
+            return{
+                messagr:"Service2 messagr"
+            }
+        }
+    });
+    //工厂方法
+    $provide.factory('factoryDome',function(){
+        //返回任何类型
+        return "asdasasd";
+    });
+    //自定义服务  返回的必须是对象 ，引用类型
+    $provide.service('serviceDome',function(){
+        return [1,2,3,4,5,6,7];
+    });
+    $filterProvider.register("filterAge",function(){
+        return function(obj){
+            var newObj=[];
+            angular.forEach(obj,function(date){
+                if(date.age>20){
+                    newObj.push(date);
+                }
+            });
+            return newObj;
+        }
+    });
+    $controllerProvider.register("filterController",function($scope){
+        $scope.date=new Date();
+    });
+});
+angular.module('myServeApp')
+    .controller('myServeController',["$scope","Service","Service2","factoryDome","serviceDome",
+        function($scope,Service,Service2,factoryDome,serviceDome){
+            $scope.name="provide";
+            console.log(Service);
+            console.log(Service2);
+            console.log(factoryDome);
+            console.log(serviceDome);
 }]);
