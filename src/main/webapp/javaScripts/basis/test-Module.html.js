7e57566d59f8a62645cbe5dc88acb72c18fd4546
zhangxd <zxxxd1001@126.com>
2016-09-23 17:14:52 +0800

    [张雪冬] 整理结构

--- src/main/webapp/javaScripts/basis/test-Module.html.js
+++
@@ -1,7 +0,0 @@
-angular.module("myFirstApp",[]);
-angular.module("myFirstApp").controller("moduleController",["$scope",function($scope){
-    $scope.name='angular.module("myApp",[]) 相当于AngularJS模块的setter方法 ,定义模块';
-    $scope.namevalue=' 声明模块,第一个是模块参数"+\n+"第二个是依赖列表' +
-        '也就是说可以被注入到模块中的对象列表 ';
-    $scope.names='angular.module("myApp") 相当于AngularJS模块的getter方法，获取模块的引用';
-}]);
--- src/main/webapp/javascripts/common/services/rcpt-resource-service.js
+++
@@ -1,9 +0,0 @@
-angular.module("hr.service").factory("rcptResource", ["$http", "hrDialog", function ($http, hrDialog) {
-    return {
-        getOutpRcptByCondition: getOutpRcptByCondition
-    };
-
-    function getOutpRcptByCondition(successCallback, errorCallback, queryParam) {
-        HrUtils.httpRequest($http, "api/rcpt/outp" + queryParam, successCallback, errorCallback, hrDialog);
-    }
-}]);
