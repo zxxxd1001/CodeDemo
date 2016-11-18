97376327bab0a504d52e20571bd1981fc644bcca
zhangxuedong <zxxxd1001@126.com>
2016-11-18 20:46:18 +0800

    [张雪冬] POI 解析excel 不成熟示例

--- src/main/java/note/benchmark/AnnotationReflectBenchmark.java
+++ src/main/java/note/benchmark/AnnotationReflectBenchmark.java
@@ -18,10 +18,10 @@ public class AnnotationReflectBenchmark {
         //回收预约号:recycleAppointNum
     }
     private static List<Map> getMethod(){
-        List<Map> methodNameList = new ArrayList<>();
+        List<Map> methodNameList = new ArrayList();
         Method[] methodLists = ScheduleJob.class.getDeclaredMethods();
         for(Method method:methodLists){
-            Map<String, String> map = new HashMap<>();
+            Map<String, String> map = new HashMap();
             //如果存在该元素的指定类型的注释，则返回这些注释，否则返回 null。
             JobReflect hrJob = method.getDeclaredAnnotation(JobReflect.class);
             if(hrJob!=null){
